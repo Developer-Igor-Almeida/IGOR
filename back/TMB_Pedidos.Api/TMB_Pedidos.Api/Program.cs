339761b2672696ef5131c2f8668571811d9639f7
Igor Almeida <igoralmeidarossi@gmail.com>
2025-10-22 21:35:10 -0300

    Docker Back

--- back/TMB_Pedidos.Api/TMB_Pedidos.Api/Program.cs
+++ back/TMB_Pedidos.Api/TMB_Pedidos.Api/Program.cs
@@ -6,6 +6,7 @@ using TMB_Pedidos.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configura DbContext
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("TMB_Connection")));
 
@@ -18,16 +19,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Use HTTPS apenas fora do container
+if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") != "true")
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+// Swagger sempre habilitado dentro do container
+if (app.Environment.IsDevelopment() || Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TMB Pedidos API V1");
+        c.RoutePrefix = string.Empty; // acessar em /
+    });
+}
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// **IMPORTANTE**: escuta em todas interfaces dentro do container
+app.Urls.Add("http://0.0.0.0:5000");
 
 app.Run();
